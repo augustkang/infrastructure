ad65be590c6fd4f60b4961ffd2edd812c8e9ffed
augustkang <iamaugustkang@gmail.com>
2021-10-16 00:10:09 +0900

    public ip assign config

--- vpc/main.tf
+++ vpc/main.tf
@@ -43,6 +43,7 @@ resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = var.public_subnets[count.index]
   availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
 
   tags = { Name = "public-subnet-${count.index}" }
 }
