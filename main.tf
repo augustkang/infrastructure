acc222c09b89cc403e4135b3ce5df788ad47469c
augustkang <iamaugustkang@gmail.com>
2022-05-21 22:19:39 +0900

    remove s3 dynamodb backend

--- main.tf
+++ main.tf
@@ -6,28 +6,6 @@ locals {
   region = "ap-northeast-2"
 }
 
-# S3 bucket for backend
-resource "aws_s3_bucket" "tfstate" {
-  bucket = "augustkang-tfstate"
-  force_destroy = true
-
-  versioning {
-    enabled = true # Prevent from deleting tfstate file
-  }
-}
-
-# DynamoDB for terraform state lock
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-lock"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 module "vpc" {
   source          = "./vpc"
   project_name    = "august"
